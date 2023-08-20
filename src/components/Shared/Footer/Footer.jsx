98dfe99ecf0960696ae142ef623a5681f5822d15
arasad1 <asaduzzamanaj07@gmail.com>
2023-08-20 10:08:57 +0600

    footer implemented

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -2,10 +2,18 @@
 
 const Footer = () => {
     return (
-        <div>
-            <h3>this is footer</h3>
-        </div>
+        <footer >
+            <div className="py-6 text-sm text-center text-gray-400">
+                © 2023 AirCNC Inc. All rights reserved.
+            </div>
+        </footer>
     );
 };
 
 export default Footer;
+
+{/* <footer className='px-4 divide-y  text-gray-800 relative bottom-0 left-0'>
+<div className='py-6 text-sm text-center text-gray-400'>
+  © 2023 AirCNC Inc. All rights reserved.
+</div>
+</footer> */}
