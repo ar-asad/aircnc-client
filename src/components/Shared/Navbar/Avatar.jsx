27cb7246d928ba096bbcd06f099fedcabb05dfde
arasad1 <asaduzzamanaj07@gmail.com>
2023-08-20 11:48:33 +0600

    navbar menudropdown implemented

--- src/components/Shared/Navbar/Avatar.jsx
+++ src/components/Shared/Navbar/Avatar.jsx
@@ -1,9 +1,16 @@
-
+import avatar from '../../../assets/images/placeholder.jpg';
 
 const Avatar = () => {
+    const user = '';
     return (
         <div>
-            <h3>this is avatar</h3>
+            <img
+                className='rounded-full'
+                src={user && user.photoURL ? user.photoURL : avatar}
+                alt="avatar"
+                width='30'
+                height='30'
+            />
         </div>
     );
 };
--- src/components/Shared/Navbar/MenuDropDown.jsx
+++ src/components/Shared/Navbar/MenuDropDown.jsx
@@ -1,11 +1,60 @@
-
+import { AiOutlineMenu } from 'react-icons/ai'
+import Avatar from './Avatar';
+import { useCallback, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const MenuDropDown = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleOpen = useCallback(() => {
+        setIsOpen(value => !value);
+    });
+    console.log(isOpen);
     return (
-        <div>
-            <h3>this is menudropdown </h3>
+        <div className="relative">
+            <div className="flex flex-row items-center gap-3">
+                {/* Aricnc btn */}
+                <div className='hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer'>
+                    Aircnc your home
+                </div>
+                {/* Dropdown btn */}
+                <div onClick={toggleOpen} className='p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-sm transition'>
+                    <AiOutlineMenu />
+                    <div className='hidden md:block'>
+                        <Avatar />
+                    </div>
+                </div>
+            </div>
+            {
+                isOpen && (
+                    <div className='absolute rounded-xl shadow-md bg-white w-[40px] md:w-3/4 right-0 top-12 text-sm overflow-hidden'>
+                        <div className='flex flex-col cursor-pointer'>
+                            <Link to='/'
+                                className='block md:hidden px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+                            >
+                                Home
+                            </Link>
+                            <div
+                                className='px-4 py-3 hover:bg-neutral-100 transition font-semibold cursor-pointer'
+                            >
+                                LogOut
+                            </div>
+                            <Link to='/login'
+                                className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+                            >
+                                Login
+                            </Link>
+                            <Link to='signup'
+                                className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+                            >
+                                Sign Up
+                            </Link>
+                        </div>
+                    </div>
+                )
+            }
         </div>
     );
 };
 
 export default MenuDropDown;
+
