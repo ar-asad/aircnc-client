1bb4f031c5de257ebc9e82270f376db1ec3772a8
arasad1 <asaduzzamanaj07@gmail.com>
2023-08-20 10:57:18 +0600

    navbar search component implemented

--- src/components/Shared/Navbar/Search.jsx
+++ src/components/Shared/Navbar/Search.jsx
@@ -1,11 +1,27 @@
-
+import { BiSearch } from 'react-icons/bi'
 
 const Search = () => {
     return (
-        <div>
-            <h3>this is searchbar</h3>
+        <div className='border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer'>
+            <div className='flex flex-row justify-between items-center'>
+                <div className='text-sm px-6 font-semibold '>
+                    Anywhere
+                </div>
+                <div className='hidden sm:block text-sm px-6 font-semibold border-x-[2px] text-center'>
+                    Any Week
+                </div>
+                <div className='text-sm flex flex-row items-center gap-3 pl-6 pr-2 text-gray-600 '>
+                    <div className='hidden sm:block'>
+                        Add Guests
+                    </div>
+                    <div className='p-2 bg-rose-500 rounded-full text-white'>
+                        <BiSearch size={18} />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
 
 export default Search;
+
