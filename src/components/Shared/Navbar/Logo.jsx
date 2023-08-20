a6d182d498dc178be09e4a78f4bfc5fb5e28e918
arasad1 <asaduzzamanaj07@gmail.com>
2023-08-20 10:30:38 +0600

    navbar logo component implemented

--- src/components/Shared/Navbar/Logo.jsx
+++ src/components/Shared/Navbar/Logo.jsx
@@ -1,11 +1,28 @@
-
+import { Link } from 'react-router-dom';
+import logoImg from '../../../assets/images/logo.png';
 
 const Logo = () => {
     return (
-        <div>
-            <h3>this is logo</h3>
-        </div>
+        <Link to='/'>
+            <img
+                className='hidden md:block'
+                src={logoImg}
+                alt="logo"
+                width='100'
+                height='100'
+            />
+        </Link>
     );
 };
 
 export default Logo;
+
+{/* <Link to='/'>
+    <img
+        className='hidden md:block'
+        src={logoImg}
+        alt='logo'
+        width='100'
+        height='100'
+    />
+</Link> */}
